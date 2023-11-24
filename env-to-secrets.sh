148e292095b728e4338378b44e6bda400063bf18
Quentin <contact@yovach.dev>
2023-11-24 17:42:57 +0100

    feat: add env vars to docker create

--- env-to-secrets.sh
+++ env-to-secrets.sh
@@ -1,2 +1,13 @@
 # Read .env.local and ignore comments and empty lines
 content=$(cat .env.local | grep "^[A-Z]")
+
+while read line; do
+  # Split line by '='
+  IFS='='
+
+  # Read line into array
+  read -ra parts <<<"$line"
+
+  # Create secret
+  printf ${parts[1]} | docker secret create ${parts[0]} -
+done <<<"$content"
